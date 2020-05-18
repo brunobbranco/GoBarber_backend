d86db2143e01b0598a6ee704db51ea6d4fde51ae
Bruno Branco <brunobbranco@gmail.com>
2020-05-17 21:22:17 -0300

    Aplicando TDD na prática

--- src/shared/container/providers/index.ts
+++ src/shared/container/providers/index.ts
@@ -3,6 +3,8 @@ import { container } from 'tsyringe';
 import DiskStorageProvider from './StorageProviders/implementations/DiskStorageProvider';
 import IStorageProvider from './StorageProviders/models/IStorageProvider';
 
+// import IMailProvider from './MailProvider/models/IMailProvider';
+
 container.registerSingleton<IStorageProvider>(
   'StorageProvider',
   DiskStorageProvider,
