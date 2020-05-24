14485d6ff7c47c2c3470c2a02314af00b5ab8472
Bruno Branco <brunobbranco@gmail.com>
2020-05-24 03:38:15 -0300

    Organizando o container

---
+++ src/shared/container/providers/StorageProviders/index.ts
@@ -0,0 +1,13 @@
+import { container } from 'tsyringe';
+
+import DiskStorageProvider from './implementations/DiskStorageProvider';
+import IStorageProvider from './models/IStorageProvider';
+
+const providers = {
+  diskStorageProvider: DiskStorageProvider,
+};
+
+container.registerSingleton<IStorageProvider>(
+  'StorageProvider',
+  providers.diskStorageProvider,
+);
